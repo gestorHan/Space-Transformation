2bd01481d190573d990d6e7d8a2de8d067a0a9ca
Jesus <Jesus>
2020-05-24 17:44:34 -0500

    Mouse Position and cursor

--- main.py
+++ main.py
@@ -148,7 +148,7 @@ if __name__ == "__main__":
     run = True
 
     while keepOpen():
-        pygame.time.delay(1000//60)
+        pygame.time.delay(1000//30)
 
         checkActions(pygame.key.get_pressed(),axis,win)
 
